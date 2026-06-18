8cd6774ced0be234ff0c8bede98a9f8a19dea119
immu <imranimmu1997@gmail.com>
2026-06-18 16:05:00 +0530

    updated backend

---
+++ Backend/C#/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,67 @@
+﻿//// Inheritance test,
+
+//using System;
+
+
+
+//class Animal
+//{
+//    public void Sleep()
+//    {
+//        Console.WriteLine("Cat is Sleepng");
+//    }
+//}
+
+//class Cat : Animal
+//{
+//    public void Meow()
+//    {
+//        Console.WriteLine("Cat is Meowing");
+//    }
+//}
+//class Program
+//{
+//    static void Main()
+//    {
+//        Cat C = new Cat();
+
+//        C.Sleep();
+//        C.Meow();
+//    }
+//}
+
+
+// private ID execution public id calling
+
+//using System;
+
+//class Student
+//{
+//    static void Id()
+//    {
+//        Console.WriteLine("ID: 245567");
+//    }
+
+//    static void Name()
+//    {
+//        Console.WriteLine("Name: Imran P");
+//    }
+
+//    public void Showing()
+//    {
+//        Id();
+//        Name();
+//    }
+//}
+
+//class Program
+//{
+//    static void Main()
+//    {
+//        Student obj1 = new Student();
+//        obj1.Showing();
+//    }
+//}
+
+
+
