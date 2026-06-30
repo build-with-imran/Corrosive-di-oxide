49f12ac32a450ca3611a6bbc183512853f31a5b4
immu <imranimmu1997@gmail.com>
2026-06-30 15:14:22 +0530

    updated

--- Backend/C#/ConsoleApp1/ConsoleApp1/obj/Debug/net10.0/ConsoleApp1.AssemblyInfo.cs
+++ Backend/C#/ConsoleApp1/ConsoleApp1/obj/Debug/net10.0/ConsoleApp1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+afa8a43199d85a471971c0fee792168b34cf8d4c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+06ceb4c16e96479f05fc6246406030bfed02ce1c")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
